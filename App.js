6339c2d34cdb83715a7cdc9bf0b61a0046a73901
“T3sultan” <incognito.soul011@gmail.com>
2022-01-21 13:05:24 +0600

    file create and package install

--- App.js
+++ App.js
@@ -1,36 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import MovieScreen from './src/screens/MovieScreen';
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{color:"red",fontSize:40,}}>Tipu Sultan</Text>
-      <View >
-        <Text style={styles.viewRound1}>React Native</Text>
-        <Text style={styles.viewRound2}>React JS</Text>
-        <Text style={styles.viewRound3}>Node JS</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name='home' 
+        component={HomeScreen} 
+        options={{headerShown:false}}
+
+        />
+        <Stack.Screen 
+        name='movie' 
+        component={MovieScreen} 
+        options={{headerShown:false}}
+
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  viewRound1:{
-    backgroundColor:'white',
-    color: 'green'
-  },
-  viewRound2:{
-    color:'blue'
-  },
-  viewRound3:{
-    color:'#895498'
-  }
-
-});
---
+++ src/screens/MovieScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+const MovieScreen = () => {
+      return (
+            <View>
+                  <Text>Movie</Text>
+            </View>
+      );
+};
+
+export default MovieScreen;
