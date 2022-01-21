c24e14e3b83b7745b54913d26a97192602ff6667
“T3sultan” <incognito.soul011@gmail.com>
2022-01-21 15:01:58 +0600

    header

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,64 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, View,Text} from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Button } from 'react-native';
+import Colors from '../constants/Colors';
 
 const HomeScreen = () => {
       return (
-            <View style={styles.container}>
-                 <Text>Home Screen </Text> 
-            </View>
+            <ScrollView style={styles.container}>
+                  <StatusBar
+                        style='auto'
+                        translucent={false}
+                        backgroundColor={Colors.white_background}
+
+                  />
+                  <View style={styles.headerContainer}>
+                        <View>
+                              <Text style={styles.headerTextStyle}>Now Showing</Text>
+                        </View>
+                        <View style={styles.buttonContainer}>
+                              <Button
+                                    style={styles.buttonText}
+                                    title='See more'
+                                    borderRadius='30px'
+
+
+                              />
+                        </View>
+
+                  </View>
+            </ScrollView>
       );
 };
-const styles=StyleSheet.create({
-      container:{
+const styles = StyleSheet.create({
+      container: {
             flex: 1,
-            justifyContent:'center',
-            alignItems:"center"
+            backgroundColor: Colors.white_background
+
+      },
+      buttonContainer: {
+            width: 45,
+            height: 20,
+            backgroundColor: "white",
+            // borderRadius: 10,
+
+      },
+      buttonText: {
+
+
+      },
+      headerContainer: {
+            flexDirection: 'row',
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 20,
+            paddingVertical: 10
+      },
+      headerTextStyle: {
+            color: '#110E47',
+            fontSize: 16,
+            fontWeight: 'bold',
+            letterSpacing: 0.02,
       }
 })
 
